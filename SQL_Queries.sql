39060189de704eb6da8d823185fc863d83e3bcc0
BrianDesirePowerful <brian.desiress@gmail.com>
2026-06-28 20:31:25 -0400

    Create SQL_Queries.sql with initial queries
    Added initial SQL queries for customer behavior analysis.

---
+++ SQL_Queries.sql
@@ -0,0 +1,12 @@
+-- Cyclistic Customer Behavior Analysis
+-- Author: Brian Desire
+
+-- Query 1: Total rides by member type
+
+-- Query 2: Average ride duration
+
+-- Query 3: Rides by day of week
+
+-- Query 4: Rideable type preferences
+
+-- SQL queries will be added here.
